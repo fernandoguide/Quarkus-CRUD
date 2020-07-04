25408fb786a3b667ced141d78facaf64bcf915e8
fernandoguide <fernando-guide@hotmail.com>
2020-07-04 12:14:29 -0300

    quarkus testes integrados com db mysql

--- src/main/java/br/com/fernandoguide/entity/Produto.java
+++ src/main/java/br/com/fernandoguide/entity/Produto.java
@@ -5,12 +5,14 @@ import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import javax.persistence.Entity;
+import javax.persistence.Table;
 import java.math.BigDecimal;
 
 import java.time.LocalDateTime;
 
 
 @Entity
+@Table(name = "Produto")
 public class Produto extends PanacheEntity {
 
     public String nome;
